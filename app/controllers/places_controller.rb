60854c5eb4ea25a617915fa7095398fd52eef8ea
Scott Ford <uofoduck14@gmail.com>
2019-04-19 18:54:01 +0000

    path form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,12 @@ class PlacesController < ApplicationController
     @places = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :address, :description)
+
 end  
