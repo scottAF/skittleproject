8b3f2a1fbcf5217793a5637006bd147a07ad95f3
Scott Ford <uofoduck14@gmail.com>
2019-04-21 14:53:49 +0000

    fuck you

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only [:new, :create]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -12,6 +13,8 @@ class PlacesController < ApplicationController
     Place.create(place_params)
     redirect_to root_path
   end
+
+  private
 
   def place_params
     params.require(:place).permit(:name, :address, :description)
