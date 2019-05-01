a921027b81d89aa489b11264b495944dc48eb33e
Scott Ford <uofoduck14@gmail.com>
2019-05-01 22:28:19 +0000

    rating visual

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'One star': '1_star',
+    'Two stars': '2_star',
+    'Three stars': '3_star',
+    'Four stars': '4_star',
+    'Five stars': '5_star'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
