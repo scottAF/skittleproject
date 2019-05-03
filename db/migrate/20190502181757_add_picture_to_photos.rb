917352b7d6ce0fbb0313fe97fbc91ae78be2fd4c
Scott Ford <uofoduck14@gmail.com>
2019-05-03 03:18:04 +0000

    update pt2

--- db/migrate/20190502181757_add_picture_to_photos.rb
+++ db/migrate/20190502181757_add_picture_to_photos.rb
@@ -1,6 +1,5 @@
 class AddPictureToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :picture, :string
-    add_index :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,13 @@ ActiveRecord::Schema.define(version: 2019_05_02_181757) do
 
   create_table "photos", force: :cascade do |t|
     t.string "caption"
+    t.integer "user_id"
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
