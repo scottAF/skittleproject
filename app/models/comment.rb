ecd94a7281e015098bca1264d20e0c50c723077a
Scott Ford <uofoduck14@gmail.com>
2019-05-03 21:25:24 +0000

    css edits

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,14 +3,15 @@ class Comment < ApplicationRecord
   belongs_to :place
 
   RATINGS = {
-    'One star': '1_star',
-    'Two stars': '2_star',
-    'Three stars': '3_star',
-    'Four stars': '4_star',
-    'Five stars': '5_star'
+    '★★★★★': '5_star',
+    '★★★★☆': '4_star',
+    '★★★☆☆': '3_star',
+    '★★☆☆☆': '2_star',
+    '★☆☆☆☆': '1_star'
   }
 
   def humanized_rating
     RATINGS.invert[self.rating]
   end
 end
+
