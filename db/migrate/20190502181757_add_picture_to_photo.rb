6dc7f97ebc060bef27fad009b15d8b60e4ec40a7
Scott Ford <uofoduck14@gmail.com>
2019-05-03 03:08:10 +0000

    update

--- db/migrate/20190502181757_add_picture_to_photo.rb
+++ db/migrate/20190502181757_add_picture_to_photo.rb
@@ -1,4 +1,4 @@
-class AddPictureToPhoto < ActiveRecord::Migration[5.2]
+class AddPictureToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :picture, :string
     add_index :user_id
